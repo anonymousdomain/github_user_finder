5b10c5bf977c675e69b384328cc9bec431d1575b
anonymousdomain <domainanonymous1@gmail.com>
2022-01-22 18:30:49 +0300

    useReducer

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,31 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
+import GithubContext from '../../context/github/GithubContext'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
-	const [users, setUsers] = useState([])
-	const [Loading, setLoading] = useState(true)
+ const {users,loading,fetchUsers}=useContext(GithubContext)
 	useEffect(() => {
 		fetchUsers()
 	}, [])
 
-	const fetchUsers = async () => {
-		const res = await fetch(`${process.env.REACT_APP_URL}/users`, {
-			headers: {
-				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-			},
-		})
-		const data = await res.json()
-		setUsers(data)
-		setLoading(false)
-	}
 
-	if (Loading) {
+	if (loading) {
 		return <Spinner/>
 	} else {
 		return (
 			<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
 				{users.map((user) => (
-					<li>{user.login}</li>
+					<UserItem key={user.id} user={user}/>
 				))}
 			</div>
 		)
